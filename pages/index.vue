2b4f1f689e45def3acfeb5f237132e20c767a163
Mostafa Zayed <143009621+MostafaZayed72@users.noreply.github.com>
2025-06-26 20:31:56 +0300

    t

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
 <template>
    <div class=" ">
       <!-- <Welcome class="pt-10" /> -->
-      <HomeCarousel class=" md:mt-10"/>
+      <HomeCarousel class=" mt-10"/>
       <CounterStats class="my-10 md:mt-20"/>
       <!-- <TopMarketer class="pt-10 md:pt-20"/> -->
       <!-- <Footer class=" mt-10 md:pt-20 py-4" /> -->
